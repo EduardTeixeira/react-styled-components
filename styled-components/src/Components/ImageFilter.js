db020c2a3284f8e08021fa637b2196a2aa339f77
unknown <eduard.teixeira@etec.sp.gov.br>
2021-08-17 14:57:32 -0300

    tema

--- styled-components/src/Components/ImageFilter.js
+++ styled-components/src/Components/ImageFilter.js
@@ -6,16 +6,16 @@ import saude from '../assets/images/saude.svg';
 import transporte from '../assets/images/transporte.svg';
 import utilidades from '../assets/images/utilidades.svg';
 
-import { Icone } from '../Components/UI';
+import { IconeTema } from '../Components/UI';
 
 export default (type) => {
 
   const Images = {
-    Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
-    Utilidades: <Icone src={utilidades} alt="Utilidades" />,
-    Transporte: <Icone src={transporte} alt="Transporte" />,
-    Saude: <Icone src={saude} alt="Saúde" />,
-    default: <Icone src={outros} alt="Outros" />,
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+    Utilidades: <IconeTema src={utilidades} alt="Utilidades" />,
+    Transporte: <IconeTema src={transporte} alt="Transporte" />,
+    Saude: <IconeTema src={saude} alt="Saúde" />,
+    default: <IconeTema src={outros} alt="Outros" />,
   }
 
   return Images[type] || Images.default;
